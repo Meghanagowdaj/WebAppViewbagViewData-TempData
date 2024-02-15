b5597c8af4c9a5badff4dc2ae375d1197d0a7b90
Megha P J <meghajayram30@gmail.com>
2024-02-15 23:21:07 +0530

    Add project files.

---
+++ WebAppViewbagViewData TempData/Controllers/EmpController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppViewbagViewData_TempData.Models;
+
+namespace WebAppViewbagViewData_TempData.Controllers
+{
+    public class EmpController : Controller
+    {
+        static List<Emp> listEmp = new List<Emp>
+
+    {
+
+        new Emp{ Id=1, Name="Sam", Salary=98000, Designation="Manager"},
+        new Emp{ Id=2, Name="Anil" , Salary=86000, Designation="Developer"},
+        new Emp{ Id=3, Name="Vini", Salary=98000, Designation="Manager"},
+        new Emp{ Id=4, Name="Viz", Salary=78000, Designation="Developer" },
+        new Emp{ Id=5, Name="Dipesh", Salary=65000, Designation="Tester"},
+    };
+
+        // GET: Emp
+        public ActionResult Index()
+        {
+            ViewBag.msg = "welcome to employee management";
+            ViewBag.noEmp = listEmp.Count;
+            return View(listEmp);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewData["msg"] = "Employee Registration";
+            return View(new Emp());
+        }
+        [HttpPost]
+        public ActionResult Create(Emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                listEmp.Add(emp);
+                TempData["tempmsg"] = "New Employee Registeration Success";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                return View(emp);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+            Emp emp = listEmp.SingleOrDefault(e => e.Id == id);
+            return View(emp);
+
+        [HttpPost]
+        public ActionResult Delete(int? id)
+        Emp emp = listEmp.SingleOrDefault(e => e.Id == id); 
+        if (emp != null)
+            ListEmp.Remove(emp);
+            return RedirectYoAction("Index");
+
+    }
+}
---
+++ WebAppViewbagViewData TempData/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppViewbagViewData_TempData.MvcApplication" Language="C#" %>
